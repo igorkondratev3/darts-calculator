8d9d312bad4d9f0917430cccd5ae5125690d2dc4
igor <igor93-333@yandex.ru>
2023-06-05 11:47:08 +0300

    adding dist

---
+++ dist/assets/errorMessage-ae978fd0.js
@@ -0,0 +1,1 @@
+import{o as s,b as r,t}from"./index-5dc47f3f.js";const a={class:"error"},c={__name:"errorMessage",props:{message:String},setup(e){return(o,_)=>(s(),r("div",a,t(e.message),1))}};export{c as default};
